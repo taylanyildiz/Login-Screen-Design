c024c25592c85029c069bf44dec4cb8907f28a01
Taylan YILDIZ <taylanyildz@outlook.com>
2021-09-04 11:20:19 +0300

    some change :D

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
