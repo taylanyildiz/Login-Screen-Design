c50fe9e38a120f3c438f6db24a43a134a7a322f8
Taylan YILDIZ <taylanyildz@outlook.com>
2021-09-04 23:25:48 +0300

    some change android side splash

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(child: Text('Home Screen')),
+    );
   }
 }
